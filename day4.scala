02473367b2520e1e8e6f295cd15add0ba914439a
James Earl Douglas <james@earldouglas.com>
2015-07-12 10:58:34 -0700

    Add (broken) applicative laws

--- day4.scala
+++ day4.scala
@@ -27,29 +27,76 @@ object Amount {
     new Functor[Amount] {
       def map[A, B](fa: Amount[A])(f: A => B): Amount[B] =
         fa match {
+          //case One(a: Int) => One(f.asInstanceOf[Int => B](42))
           case One(a) => One(f(a))
           case Couple(a, b) => Couple(f(a), f(b))
           case Few(a, b, c) => Few(f(a), f(b), f(c))
         }
     }
 
-  val gen: Gen[Amount[Int]]=
-    for {
-      x <- Arbitrary.arbitrary[Int]
-      y <- Arbitrary.arbitrary[Int]
-      z <- Arbitrary.arbitrary[Int]
-      a <- Gen.oneOf(One(x), Couple(x,y), Few(x,y,z))
-    } yield a
+  implicit val applicative: Applicative[Amount] =
+    new Applicative[Amount] {
+      def point[A](a: => A): Amount[A] = One(a)
+      def ap[A, B](fa: => Amount[A])(f: => Amount[A => B]): Amount[B] =
+        fa match {
+          case One(a) =>
+            f match {
+              case One(g) => One(g(a))
+              case Couple(g, _) => One(g(a))
+              case Few(g, _, _) => One(g(a))
+            }
+          case Couple(a, b) =>
+            f match {
+              case One(g) => Couple(g(a), g(b))
+              case Couple(g, _) => Couple(g(a), g(b))
+              case Few(g, _, _) => Couple(g(a), g(b))
+            }
+          case Few(a, b, c) =>
+            f match {
+              case One(g) => Few(g(a), g(b), g(c))
+              case Couple(g, _) => Few(g(a), g(b), g(c))
+              case Few(g, h, i) => Few(g(a), h(b), i(c))
+            }
+        }
+    }
 
-  implicit val arbitrary: Arbitrary[Amount[Int]] =
-    Arbitrary(gen)
+    val genAmountInt: Gen[Amount[Int]]=
+      for {
+        x <- Gen.choose(-100,100)
+        y <- Gen.choose(-100,100)
+        z <- Gen.choose(-100,100)
+        a <- Gen.oneOf(One(x), Couple(x,y), Few(x,y,z))
+      } yield a
+
+    implicit val arbitraryAmountInt: Arbitrary[Amount[Int]] =
+      Arbitrary(genAmountInt)
+
+    val genAmountIntInt: Gen[Amount[Int => Int]]=
+      for {
+        x <- Arbitrary.arbitrary[Int => Int]
+        y <- Arbitrary.arbitrary[Int => Int]
+        z <- Arbitrary.arbitrary[Int => Int]
+        a <- Gen.oneOf(One(x), Couple(x,y), Few(x,y,z))
+      } yield a
+
+    implicit val arbitraryAmountIntToInt: Arbitrary[Amount[Int => Int]] =
+      Arbitrary(genAmountIntInt)
 
 }
 
 object Main extends App {
 
-  // Functor
+  // Functor laws
+  println("## Functor laws")
+  println("")
   functor.laws[Amount].check
+  println("")
+
+  // Applicative laws
+  println("## Applicative laws")
+  println("")
+  applicative.laws[Amount].check
+  println("")
 
 }
 
